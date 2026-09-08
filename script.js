39e1bb072bdb69b5611817204596e9bd434b50ed
Nancy070278 <nancy.winkelmolen@home.nl>
2026-09-08 22:25:22 +0200

    Add payment links and limit jar purchase to 6

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 let amount = 1;
 const pricePerJar = 7;
+const maxJars = 6;
+
+const paymentLinks = {
+    1: "https://betaalverzoek.rabobank.nl/betaalverzoek/?id=Q6x2tPh8RvOTEBzcDO1_mA",
+    2: "https://betaalverzoek.rabobank.nl/betaalverzoek/?id=J9oLDlCLQkSyeAqS5MMDoQ",
+    3: "https://betaalverzoek.rabobank.nl/betaalverzoek/?id=sd_LnXVmQROQt_zN8E5hkg",
+    4: "https://betaalverzoek.rabobank.nl/betaalverzoek/?id=Crc2XZQaR42rME_PDw9Tyw",
+    5: "https://betaalverzoek.rabobank.nl/betaalverzoek/?id=c2rPMQEPTX2xN3kOjdQOiA",
+    6: "https://betaalverzoek.rabobank.nl/betaalverzoek/?id=PXo7WHllTGC73kXGdoUndQ"
+};
 
 function update() {
     document.getElementById("amount").innerText = amount;
@@ -10,12 +20,16 @@ function update() {
     document.getElementById("price").innerText = totalText;
 
     document.querySelector(".paybutton").innerText =
-        "💳 Betaal " + totalText;
+        "💛 Betaal " + totalText;
 }
 
 function plus() {
-    amount++;
-    update();
+    if (amount < maxJars) {
+        amount++;
+        update();
+    } else {
+        alert("Er zijn maximaal 6 potten beschikbaar.");
+    }
 }
 
 function minus() {
@@ -25,4 +39,20 @@ function minus() {
     }
 }
 
-update();
+document.addEventListener("DOMContentLoaded", function () {
+
+    update();
+
+    document.querySelector(".paybutton").addEventListener("click", function () {
+
+        const link = paymentLinks[amount];
+
+        if (link) {
+            window.open(link, "_blank");
+        } else {
+            alert("Betaallink niet beschikbaar.");
+        }
+
+    });
+
+});
